67f73bf92432ddd09cc423b436ac4f99f38666c1
Ochoagram <ricardogochoa@me.com>
2022-05-14 21:41:48 -0500

    Succes page 1/2

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,49 +1,119 @@
 import React from 'react'
-import { Grid, GridItem, VStack, Container, Box, Image, Stack, Text, Button, UnorderedList, ListItem, ListIcon } from '@chakra-ui/react';
+import { Grid, GridItem, VStack, Container, Box, Image, Stack, Text, Button, UnorderedList, ListItem, ListIcon, Flex, AccordionItem, AccordionButton, AccordionIcon, AccordionPanel, Accordion, AspectRatio } from '@chakra-ui/react';
 import Link from 'next/link';
 
 export const success = () => {
   return (
-      <Stack mt={40}>
-        <Stack  mb={-8} display="flex" justifyContent="center" alignItems="center">
-            <Text className='tittlesuccess' position={"absolute"} zIndex={3}> Compra Exitosa </Text>
-            <Image
-           
-            w="290px"
-            src='https://res.cloudinary.com/dnxxkvpiz/image/upload/v1652156945/Socialemediakit/tape_aoo4or.png'
-            alt='success'
-            zIndex={2}
-            />
-            
-        </Stack>  
+      <Stack mt={{lg:20, md:10}} >
 
-        <VStack alignContent={"center"}>
-            <Box  boxSize='sm' bgColor="white" borderRadius={"2rem"} w="410px" >
+        <Flex alignItems={"center"} justifyContent={"center"} p={5}
+        display={{ sm: "flex" }} direction={{ lg:"row", sm: "column-reverse" }}
+        >
+
+        <VStack >
+            <Box  boxSize='sm' bgColor="white" className='SuccessContainerInfo' maxW={"600px"} w={"100%"} h={{lg:"650px",md:"100%",sm:"150%"}}
+                p={{lg:14, md:10, sm:8}} 
+
+                >
+                <Link href="/" passHref>
+                    <Flex direction={"row"} cursor={"pointer"}>
+                        <span>
+                                <Image mr={1} src='https://icongr.am/octicons/arrow-left.svg?size=25&color=currentColor' alt='Back'/>
+                        </span> 
+                        <a>
+                            Regresar / Back
+                        </a>
+                    </Flex>
+                </Link>
+
+                <Text as={"h1"} fontWeight={"600"} fontSize={{lg:'5xl', sm:"3xl"}} mt={5} lineHeight={"2.5rem"}>Felicidades / Congratulations</Text>
                 <Text
-                w={"100%"}
-                pl={10}
-                pt={16}
-                pr={10}
-                >Se envió un email con el acceso a tus plantillas, no olvides revisar la bandeja de spam.</Text>
+                mt={5}
+                mb={5}
+                >Tras el pago, la dirección de correo electrónico que utilizó para realizarlo recibirá un enlace para descargar sus archivos, no olvides revisar la bandeja de spam.</Text>
                 
                 <Text
-                w={"100%"}
-                pl={10}
-                pt={3}
-                >Cualquier duda contacta en: </Text>
+                mb={10}
+                >After payment, the email address you used to make your payment will receive a link to download your files, don't forget to check your spam folder.
+                </Text>
 
-                <UnorderedList pl={10} pt={3} >
-                <ListItem><a href='mailto:socialmkit@gmail.com'> socialmkit@gmail.com </a></ListItem>
-                <ListItem><a href='https://wa.me/529931433105'  target="_blank" rel="noreferrer"> WhatsApp </a></ListItem>
-                </UnorderedList>
 
-                <Button alignSelf="flex-end" ml={10} mt={10}>
-                    <Link href="/" passHref>
-                        <a>Regresar al Home</a>
-                    </Link></Button>
+               
+                <Accordion className='acordion1' allowMultiple mt={"6"} mb={"1"} bgColor={"#f1f1f1"}>
+                    <AccordionItem border={"none"}>
 
+                    <Text>
+                    <AccordionButton>
+                        <Box flex='1' textAlign='left' className='acordionTitle'>
+                            Contact for questions and suggestions
+                        </Box>
+                        <AccordionIcon />
+                    </AccordionButton>
+
+                    </Text>
+
+                    <AccordionPanel pb={4}>
+                    <UnorderedList >
+                        <ListItem mb={6} mt={6} display={"flex"} flexDirection={"row"}><a href='mailto:socialmkit@gmail.com'> socialmkit@gmail.com </a> <span><Image src='https://icongr.am/feather/arrow-up-right.svg?size=20&color=000000' alt='email contact'></Image></span></ListItem>
+                        <ListItem  display={"flex"} flexDirection={"row"}><a href='https://wa.me/529931433105'  target="_blank" rel="noreferrer"> WhatsApp </a><span><Image src='https://icongr.am/feather/arrow-up-right.svg?size=20&color=000000' alt='email contact'></Image></span></ListItem>
+                    </UnorderedList>
+                    </AccordionPanel>
+                    </AccordionItem>
+                </Accordion>
+
+                <Accordion className='acordion2' allowMultiple  bgColor={"#f1f1f1"}>
+                    <AccordionItem border={"none"}>
+
+                    <Text>
+                    <AccordionButton >
+                        <Box flex='1' textAlign='left' className='acordionTitle'>
+                            Contacto para dudas o sugerencias:
+                        </Box>
+                        <AccordionIcon />
+                    </AccordionButton>
+
+                    </Text>
+
+                    <AccordionPanel pb={4}>
+                        <UnorderedList >
+                            <ListItem mb={6} mt={6} display={"flex"} flexDirection={"row"}><a href='mailto:socialmkit@gmail.com'> socialmkit@gmail.com </a> <span><Image src='https://icongr.am/feather/arrow-up-right.svg?size=20&color=000000' alt='email contact'></Image></span></ListItem>
+                            <ListItem  display={"flex"} flexDirection={"row"}><a href='https://wa.me/529931433105'  target="_blank" rel="noreferrer"> WhatsApp </a><span><Image src='https://icongr.am/feather/arrow-up-right.svg?size=20&color=000000' alt='email contact'></Image></span></ListItem>
+                        </UnorderedList>
+                    </AccordionPanel>
+                    </AccordionItem>
+                </Accordion>
             </Box>
         </VStack>
+
+        <VStack 
+        className='SuccessContainerImage' 
+        maxW={"600px"}
+        h={{ lg:"650px", sm:"300px" }}
+        >
+            <Box h={"650px"} >
+                
+                <Image objectFit={"cover"} h={"100%"} 
+                src='https://res.cloudinary.com/dnxxkvpiz/image/upload/v1652463763/Socialemediakit/main_success_xhgjhd.jpg'
+                alt='SMK Red' />
+                
+            </Box>
+        </VStack>
+
+        </Flex>
+
+        <Stack 
+        className='franja' 
+        overflow={"hidden"} 
+        fontSize="30px" 
+        textTransform={"uppercase"} 
+        bgColor={"black"}
+        
+        >
+            <Flex>
+                <Text flexShrink={"unset"} w="100%" whiteSpace={"nowrap"} className="textanimated" >Disfruta tus plantillas · Enjoy your templates · Disfruta tus plantillas · Enjoy your templates · Disfruta tus plantillas · Enjoy your templates · Disfruta tus plantillas · Enjoy your templates · Disfruta tus plantillas · Enjoy your templates ·</Text>
+            </Flex>
+        </Stack>
+
 
       </Stack>
   )
