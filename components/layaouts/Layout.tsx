15d353f408464870c95c4038c34674c5f49d872d
Ochoagram <ricardogochoa@me.com>
2022-05-31 13:28:42 -0500

    metatag gif test

--- components/layaouts/Layout.tsx
+++ components/layaouts/Layout.tsx
@@ -29,11 +29,12 @@ export const Layout: FC<Props> = ({ children, title, color }) => {
           {/* Facebook <!-- Open Graph data --> */}
 
           <meta property="og:url"                content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
-          <meta property="og:type"               content="website" />
+          <meta property="og:type"               content="video.other" />
           <meta property="og:title"              content="Kit para redes sociales" />
           <meta property="og:description"        content="¡Un AÑO de contenido de redes sociales hecho por ti!, Comparte tu contenido único y atractivo todos los días." />
           <meta property="og:image"              content="https://res.cloudinary.com/dnxxkvpiz/image/upload/v1654019782/Socialemediakit/smkit_nin0sx.gif" />
-
+          <meta property="og:image:width"       content="1200"/>
+          <meta property="og:image:height"       content="628"/>
 
           <meta name="twitter:card" content="summary"/>
           <meta name="twitter:site" content="@ochoagram"/>
